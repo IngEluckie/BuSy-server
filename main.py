826e4114c37b7785220c43f19755bca77314290d
IngEluckie <ing.eluckie@gmail.com>
2025-12-19 12:55:59 -0600

    Primer guardado

---
+++ main.py
@@ -0,0 +1,14 @@
+# main.py
+
+"""
+BUSINESS SYSTEM (BuSy)
+
+A customized system designed for short business that 
+need to automate and do regular stuff. This system
+has been developed for a children's boutique.
+- 4 shops (POS).
+- 1 online shop.
+- Inventory synchronization.
+- Self managed static files: Stored media and databases.
+"""
+
