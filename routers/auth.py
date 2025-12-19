269697339e03ab85aa8fb5b8e00bd840b2e5b921
IngEluckie <ing.eluckie@gmail.com>
2025-12-19 17:31:06 -0600

    Singleton para base de datos agregado.

--- routers/auth.py
+++ routers/auth.py
@@ -3,8 +3,20 @@
 # Import libraries
 from fastapi import APIRouter
 from pydantic import BaseModel
+from datetime import datetime
 
 auth: APIRouter = APIRouter(prefix="/authentication")
+
+class User(BaseModel):
+    id: int
+    username: str | None # En su defecto se puede usar el fullname
+    fullname: str
+    birthday: datetime | None
+    rfc: str | None
+    cellphone: int | None
+
+class PrivateUser(User):
+    pw: str
 
 @auth.get
 async def ison():
