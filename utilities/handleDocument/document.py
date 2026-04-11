e75cd02647e701bd3d9da0a52a0f18245ae828ea
IngEluckie <ing.eluckie@gmail.com>
2026-04-10 19:27:31 -0600

    Guardado provisional

---
+++ utilities/handleDocument/document.py
@@ -0,0 +1,27 @@
+# document.py
+
+
+
+
+
+class Document:
+
+    def __init__(self, doc_name: str) -> None:
+        self.doc_name = doc_name
+
+    def retrieveDoc(self,):
+        # Si el archivo no existe, que lo cree
+        try:
+            pass
+        except Exception as e:
+            print("Ups error")
+        pass
+
+    def write(self,):
+        pass
+
+    def saveDoc(self,):
+        pass
+
+    def _createDoc(self,):
+        pass
